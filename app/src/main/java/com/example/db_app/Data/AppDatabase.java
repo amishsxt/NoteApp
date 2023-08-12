feeaff32faf2910d8471a13d8c8167b66c4675f0
amish <amishsxt147@gmail.com>
2023-08-12 15:20:34 +0530

    updates

--- app/src/main/java/com/example/db_app/Data/AppDatabase.java
+++ app/src/main/java/com/example/db_app/Data/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = Note.class,exportSchema = false, version = 1)
+@Database(entities = Note.class,exportSchema = false, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final String db_name = "NotesDataBase";
